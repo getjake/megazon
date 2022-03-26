1931cab24d4cfe50154772eaa35693c2e51e62bf
jake <bozi.chinese@gmail.com>
2022-03-26 10:30:28 +0800

    product router

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Layout from '../components/Layout'
-import { Grid, Card, CardActionArea, CardMedia,CardContent,Typography,CardActions, Button } from '@material-ui/core'
-import data from '../utils/data'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import NextLink from "next/link";
+import Layout from "../components/Layout";
+import { Grid, Card, CardActionArea, CardMedia, CardContent, Typography, CardActions, Button } from "@material-ui/core";
+import data from "../utils/data";
 
 export default function Home() {
   return (
@@ -14,26 +15,24 @@ export default function Home() {
         {data.products.map((product) => (
           <Grid item md={4} key={product.name}>
             <Card>
-              <CardActionArea>
-                <CardMedia 
-                  component='img'
-                  image={product.image}
-                  title={product.name}
-                >
-
-                </CardMedia>
-                <CardContent>
-                <Typography>{product.name}</Typography>
-                </CardContent>
-              </CardActionArea>
+              <NextLink href={`/product/${product.slug}`} passHref>
+                <CardActionArea>
+                  <CardMedia component="img" image={product.image} title={product.name} alt={product.name}></CardMedia>
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                  </CardContent>
+                </CardActionArea>
+              </NextLink>
               <CardActions>
                 <Typography>${product.price}</Typography>
-                <Button size='small' color='primary'>Add to Cart</Button>
+                <Button size="small" color="primary">
+                  Add to Cart
+                </Button>
               </CardActions>
             </Card>
           </Grid>
         ))}
       </Grid>
     </Layout>
-  )
+  );
 }
