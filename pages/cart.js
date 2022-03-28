a366bc5b2eed8bf350a52e52261eeb94b40c3fdc
jake <bozi.chinese@gmail.com>
2022-03-28 08:26:44 +0800

    use client-side rendering for the cart

--- pages/cart.js
+++ pages/cart.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import dynamic from 'next/dynamic'
 import Layout from "../components/Layout";
 import { Store } from "../utils/Store";
 import NextLink from "next/link";
@@ -94,7 +95,7 @@ const CartScreen = () => {
             </TableContainer>
           </Grid>
 
-          <Grid md={3} xs={12} >
+          <Grid item md={3} xs={12} >
             <Card>
               <List>
                 <ListItem>
@@ -117,4 +118,4 @@ const CartScreen = () => {
   );
 };
 
-export default CartScreen;
+export default dynamic(()=> Promise.resolve(CartScreen), {ssr: false});
