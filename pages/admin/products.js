b18bff5fda16806ac2b33576d3aa81096bc45716
jake <bozi.chinese@gmail.com>
2022-04-06 23:31:41 +0800

    create and delete product for admin

--- pages/admin/products.js
+++ pages/admin/products.js
@@ -19,6 +19,7 @@ import {
   TableCell,
   TableBody,
 } from "@mui/material";
+import { useSnackbar } from "notistack";
 import { getError } from "../../utils/error";
 import { Store } from "../../utils/Store";
 import Layout from "../../components/Layout";
@@ -32,6 +33,20 @@ const reducer = (state, action) => {
       return { ...state, loading: false, products: action.payload, error: "" };
     case "FETCH_FAIL":
       return { ...state, loading: false, error: action.payload };
+    case "CREATE_REQUEST":
+      return { ...state, loadingCreate: true };
+    case "CREATE_SUCCESS":
+      return { ...state, loadingCreate: false };
+    case "CREATE_FAIL":
+      return { ...state, loadingCreate: false };
+    case "DELETE_REQUEST":
+      return { ...state, loadingDelete: true };
+    case "DELETE_SUCCESS":
+      return { ...state, loadingDelete: false, successDelete: true };
+    case "DELETE_FAIL":
+      return { ...state, loadingDelete: false };
+    case "DELETE_RESET":
+      return { ...state, loadingDelete: false, successDelete: false };
     default:
       state;
   }
@@ -42,12 +57,54 @@ const AdminProduct = () => {
   const router = useRouter();
   const classes = useStyles();
   const { userInfo } = state;
+  const { enqueueSnackbar } = useSnackbar();
+  const [{ loading, error, products, loadingCreate, successDelete, loadingDelete }, dispatch] =
+    useReducer(reducer, {
+      loading: true,
+      products: [],
+      error: "",
+    });
 
-  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
-    loading: true,
-    products: [],
-    error: "",
-  });
+  const createHandler = async () => {
+    if (!window.confirm("Are you sure to create a new product?")) {
+      return;
+    }
+
+    try {
+      dispatch({ type: "CREATE_REQUEST" });
+      const { data } = await axios.post(
+        `/api/admin/products`,
+        {},
+        {
+          headers: { authorization: `Bearer ${userInfo.token}` },
+        }
+      );
+      dispatch({ type: "CREATE_SUCCESS" });
+      enqueueSnackbar("Product created successfully", { variant: "success" });
+      router.push(`/admin/product/${data.product._id}`);
+    } catch (error) {
+      dispatch({ type: "CREATE_FAIL" });
+      enqueueSnackbar(getError(error), { variant: "error" });
+    }
+  };
+
+  const deleteHandler = async (productId) => {
+    if (!window.confirm("Are you sure to delete this product?")) {
+      return;
+    }
+
+    try {
+      dispatch({ type: "DELETE_REQUEST" });
+      await axios.delete(`/api/admin/products/${productId}`, {
+        headers: { authorization: `Bearer ${userInfo.token}` },
+      });
+      dispatch({ type: "DELETE_SUCCESS" });
+      enqueueSnackbar("Product deleted successfully", { variant: "success" });
+    } catch (error) {
+      dispatch({ type: "DELETE_FAIL" });
+      enqueueSnackbar(getError(error), { variant: "error" });
+    }
+  };
 
   useEffect(() => {
     if (!userInfo) {
@@ -64,8 +121,12 @@ const AdminProduct = () => {
         dispatch({ type: "FETCH_FAIL", payload: getError(err) });
       }
     };
-    fetchData();
-  }, []);
+    if (successDelete) {
+      dispatch({ type: "DELETE_RESET" });
+    } else {
+      fetchData();
+    }
+  }, [successDelete]);
 
   return (
     <Layout title="Products">
@@ -95,9 +156,20 @@ const AdminProduct = () => {
           <Card className={classes.section}>
             <List>
               <ListItem>
-                <Typography component="h1" variant="h1">
-                  Products
-                </Typography>
+                <Grid container alignItems="center">
+                  <Grid item xs={6}>
+                    <Typography component="h1" variant="h1">
+                      Products
+                    </Typography>
+                    {loadingDelete && <CircularProgress />}
+                  </Grid>
+                  <Grid item xs={5} align="right">
+                    <Button onClick={createHandler} color="primary" variant="contained">
+                      Create
+                    </Button>
+                    {loadingCreate && <CircularProgress />}
+                  </Grid>
+                </Grid>
               </ListItem>
 
               <ListItem>
@@ -125,20 +197,23 @@ const AdminProduct = () => {
                             <TableCell>{product._id.substring(20, 24)}</TableCell>
                             <TableCell>{product.name}</TableCell>
                             <TableCell>${product.price}</TableCell>
-                            <TableCell>${product.category}</TableCell>
-                            <TableCell>
-                              {product.countInStock}
-                            </TableCell>
-                            <TableCell>
-                              {product.rating}
-                            </TableCell>
+                            <TableCell>{product.category}</TableCell>
+                            <TableCell>{product.countInStock}</TableCell>
+                            <TableCell>{product.rating}</TableCell>
                             <TableCell>
                               <NextLink href={`/admin/product/${product._id}`} passHref>
-                                <Button size="small" variant="contained">EDIT</Button>
+                                <Button size="small" variant="contained">
+                                  EDIT
+                                </Button>
                               </NextLink>
-                              <NextLink href={`/admin/product/${product._id}`} passHref>
-                                <Button size="small" variant="contained">DELETE</Button>
-                              </NextLink>
+
+                              <Button
+                                size="small"
+                                variant="contained"
+                                onClick={() => deleteHandler(product._id)}
+                              >
+                                DELETE
+                              </Button>
                             </TableCell>
                           </TableRow>
                         ))}
